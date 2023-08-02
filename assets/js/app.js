dbbbec9c23993fc5b93362b5c28b86c29317c122
Francisco Motta <motta.lopes@hotmail.com>
2023-08-02 04:02:06 -0300

    lightGallery implementation, livros HTML

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+lightGallery(document.getElementById('lightgallery-livros'), {
+    plugins: [lgZoom],
+    licenseKey: '0000-0000-0000-0000',
+    speed: 500,
+    thumbnail: true,
+    // ... other settings
+});
+lightGallery(document.getElementById('lightgallery-livros-2'), {
+    plugins: [lgZoom],
+    licenseKey: '0000-0000-0000-0000',
+    speed: 500,
+    thumbnail: true,
+    // ... other settings
+});
