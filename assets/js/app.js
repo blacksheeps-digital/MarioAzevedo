09a9a0832d8105e901c0d69eca452101b6fd22ca
Francisco Motta <motta.lopes@hotmail.com>
2023-08-04 22:36:32 -0300

    header mob fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,21 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// document.addEventListener("DOMContentLoaded", function () {
+//     var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 300) {
-            scrollToTopBtn.style.display = "block";
-        } else {
-            scrollToTopBtn.style.display = "none";
-        }
-    });
+//     window.addEventListener("scroll", function () {
+//         if (window.scrollY > 300) {
+//             scrollToTopBtn.style.display = "block";
+//         } else {
+//             scrollToTopBtn.style.display = "none";
+//         }
+//     });
 
-    scrollToTopBtn.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
-});
+//     scrollToTopBtn.addEventListener("click", function () {
+//         window.scrollTo({
+//             top: 0,
+//             behavior: "smooth"
+//         });
+//     });
+// });
 
 lightGallery(document.getElementById('lightgallery-livros'), {
     plugins: [lgZoom],
