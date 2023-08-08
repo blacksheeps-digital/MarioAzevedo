d404a2e7f9c882e0232c8d11adb46ca4c4cfbb2a
Mateus Batatinha <mateuslc00@gmail.com>
2023-08-08 00:01:22 -0300

    new - ok

--- wp-content/themes/panda/template-parts/content.php
+++ wp-content/themes/panda/template-parts/content.php
@@ -25,36 +25,73 @@
                         endif;
 
                         ?>
+                    </header>
+                    <?php panda_post_thumbnail(); ?>
 
-                        <?php panda_post_thumbnail(); ?>
+                    <div class="entry-content">
+                        <?php
+                        the_content(
+                            sprintf(
+                                wp_kses(
+                                    /* translators: %s: Name of current post. Only visible to screen readers */
+                                    __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'panda'),
+                                    array(
+                                        'span' => array(
+                                            'class' => array(),
+                                        ),
+                                    )
+                                ),
+                                wp_kses_post(get_the_title())
+                            )
+                        );
 
-                        <div class="entry-content">
-                            <?php
-                            the_content(
-                                sprintf(
-                                    wp_kses(
-                                        /* translators: %s: Name of current post. Only visible to screen readers */
-                                        __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'panda'),
-                                        array(
-                                            'span' => array(
-                                                'class' => array(),
-                                            ),
-                                        )
-                                    ),
-                                    wp_kses_post(get_the_title())
-                                )
-                            );
+                        wp_link_pages(
+                            array(
+                                'before' => '<div class="page-links">' . esc_html__('Pages:', 'panda'),
+                                'after'  => '</div>',
+                            )
+                        );
+                        ?>
+                    </div>
+                    <?php
+                    $id_post = $post->ID;
 
-                            wp_link_pages(
-                                array(
-                                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'panda'),
-                                    'after'  => '</div>',
-                                )
-                            );
-                            ?>
-                        </div>
+                    // Obter categorias do post atual
+                    $categories = wp_get_post_categories($id_post);
+
+                    $q = new WP_Query(
+                        array(
+                            'post_type' => 'post',
+                            'showposts' => 2,
+                            'post__not_in' => array($id_post),
+                            'category__in' => $categories, // Mostrar posts da categoria do post atual
+                        )
+                    );
+
+                    if ($q->have_posts()) :
+                        while ($q->have_posts()) : $q->the_post();
+                    ?>
+                            <div class="related-post">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <div class="related-thumbnail">
+                                        <a href="<?php the_permalink(); ?>">
+                                            <?php the_post_thumbnail('medium'); ?>
+                                        </a>
+                                    </div>
+                                <?php endif; ?>
+                                <div class="related-content">
+                                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                    <p><?php the_excerpt(); ?></p>
+                                </div>
+                            </div>
+                    <?php
+                        endwhile;
+                    endif;
+                    wp_reset_postdata();
+                    ?>
+
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-3" id="sidebar">
                     <?php custom_display_categories(); ?>
                     <h3>Arquivos</h3>
                     <ul>
